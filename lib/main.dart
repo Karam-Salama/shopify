5c88bc8cb93f2b2a0e4ce717cbac4068f739e8cb
Karam-Salama <karamslama244@gmail.com>
2024-11-14 23:31:45 +0200

    add icon and name for my app and make some things in code

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shopify/firebase_options.dart';
-import 'core/functions/onGenerate_routes.dart';
-import 'core/utils/app_theme.dart';
-import 'modules/splash/pages/splash.dart';
+import 'app/shopify_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const Shopify());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: onGenerateRoute,
-      initialRoute: SplashPage.routeName,
-    );
-  }
-}
